f56b4567d161d44d9ae9c520ae0e2ab5d48142fa
shonsojan <shonsojan2004@gmail.com>
2025-03-09 17:15:32 -0400

    edited

--- includes/connect_general.php
+++ includes/connect_general.php
@@ -1,5 +1,5 @@
 <?php
-$dsn = "mysql:host=localhost:8889;dbname=lmkxq389_shon_portfolio;charset=utf8mb4";
+$dsn = "mysql:host=localhost;dbname=lmkxq389_shon_portfolio;charset=utf8mb4";
 try {
 $connection = new PDO($dsn, 'lmkxq389_general', '4qo%_tg^3#v)');
 } catch (Exception $e) {
