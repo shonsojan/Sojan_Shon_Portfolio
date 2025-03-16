406a569f9f887377a770ba74db67ab63a945dd81
shonsojan <shonsojan2004@gmail.com>
2025-03-16 17:43:40 -0400

    edited connect

--- includes/connect_general.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dsn = "mysql:host=localhost;dbname=lmkxq389_shon_portfolio;charset=utf8mb4";
-try {
-$connection = new PDO($dsn, 'lmkxq389_general', '4qo%_tg^3#v)');
-} catch (Exception $e) {
-  error_log($e->getMessage());
-  exit('unable to connect');
-}
-?>
